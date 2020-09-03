15507a828fcf0fe3d483c7aa592c7a8a9e36b03d
mauroef <cypress90@gmail.com>
2020-09-03 01:06:38 -0300

    71. Combining Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,42 @@
 import 'package:flutter/material.dart';
 
-import './quiz.dart';
-import './result.dart';
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
+void main() {
+  runApp(MyApp());
 }
 
-class _MyAppState extends State<MyApp> {
-  final _questions = const [
-    {
-      'questionText': 'What\'s your favorite color?',
-      'answers': [
-        {'text': 'Blue', 'score': 1},
-        {'text': 'Yellow', 'score': 7}
-      ]
-    },
-    {
-      'questionText': 'What\'s your favorite animal?',
-      'answers': [
-        {'text': 'Lion', 'score': 4},
-        {'text': 'Tiger', 'score': 2},
-        {'text': 'Horse', 'score': 9},
-        {'text': 'Monkey', 'score': 5},
-        {'text': 'snake', 'score': 8}
-      ]
-    },
-    {
-      'questionText': 'Who\'s your favorite instructor?',
-      'answers': [
-        {'text': 'Max', 'score': 9},
-        {'text': 'Jonh', 'score': 8},
-        {'text': 'David', 'score': 7}
-      ]
-    }
-  ];
-  var _questionIndex = 0;
-  var _totalScore = 0;
-
-  void _resetQuiz() {
-    setState(() {
-      _questionIndex = 0;
-      _totalScore = 0;
-    });
-  }
-
-  void _answerQuestion(int score) {
-    _totalScore += _totalScore;
-    setState(() {
-      _questionIndex = _questionIndex + 1;
-    });
-    print(_questionIndex);
-    if (_questionIndex < _questions.length) {
-      print('We have more questions!');
-    } else {
-      print('No more questions');
-    }
-  }
-
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('My First App'),
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(child: Text('Flutter App')),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('CHART!'),
+              elevation: 5,
             ),
-            body: _questionIndex < _questions.length
-                ? Quiz(
-                    questions: _questions,
-                    questionIndex: _questionIndex,
-                    answerQuestion: _answerQuestion)
-                : Result(_totalScore, _resetQuiz)));
+          ),
+          Card(
+            child: Text('LIST OF TX'),
+          )
+        ],
+      ),
+    );
   }
 }
